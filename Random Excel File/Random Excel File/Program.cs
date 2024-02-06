83427ad64f3ced55fcf72d63edc516121b375b13
NeonLiryan <67391825+NeonLiryan@users.noreply.github.com>
2024-02-06 22:15:12 +0100

    FirstAtemptToCreateExcelFile
    First Atempt To Create Excel File

---
+++ Random Excel File/Random Excel File/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Office.Interop.Excel;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Random_Excel_File
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create folder in the app path for creating Excel file inside 
+            var systemPath = System.Environment. GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            var complete = Path.Combine(systemPath, "RandomExcel");
+
+            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+            Workbook workbook;
+            Worksheet worksheet;
+
+            workbook = excel.Workbooks.Open(complete);
+            worksheet = workbook.Worksheets[0];
+
+            Range cellRange = worksheet.Range["A1:D1"];
+            string[] strings = new[] {"test1", "testDwa", "testyTrzy","to powinno być cztery"};
+
+            cellRange.set_Value(XlRangeValueDataType.xlRangeValueDefault, strings);
+
+            workbook.SaveAs("RandomExcelFile");
+            workbook.Close();
+
+            Process.Start("RandomExcelFIle");
+        }
+    }
+}
