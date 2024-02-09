0a3adfb0f562d32fc71cd4ee2ec291cad881de63
NeonLiryan <67391825+NeonLiryan@users.noreply.github.com>
2024-02-09 15:10:20 +0100

    Beta 0.3
    Creating folder for Excel file, setting headlines for columns

--- Random Excel File/Random Excel File/Program.cs
+++ Random Excel File/Random Excel File/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.Office.Interop.Excel;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,25 +12,47 @@ namespace Random_Excel_File
     {
         static void Main(string[] args)
         {
-            //Create folder in the app path for creating Excel file inside 
-            var systemPath = System.Environment. GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-            var complete = Path.Combine(systemPath, "RandomExcel");
+            //Creating folder in the app path for creating Excel file inside 
+            string directoryPath = AppDomain.CurrentDomain.BaseDirectory;
+            string folderName = "RandomExcelFolder";
+            string folderPath = Path.Combine(directoryPath, folderName);
 
+            try
+            {
+                //Attempt to create the directory
+                Directory.CreateDirectory(folderPath);
+                Console.WriteLine("Folder created successfully." + folderPath);
+            }
+            catch (Exception ex)
+            {
+                //Handling any exceptions that may occur
+                Console.WriteLine("Error creating folder in directory: " + folderPath + " " + ex.Message);
+            }
+
+            //Creating instance of Excel application
             Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
             Workbook workbook;
             Worksheet worksheet;
 
+            //Creating worksheet in Excel application 
             workbook = excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
             worksheet = (Worksheet)workbook.Worksheets[1];
 
             worksheet.Name = "RandomExcelFile";
 
-            Range cellRange = worksheet.Range["A1:D1"];
-            string[] strings = new[] {"test1", "testDwa", "testyTrzy","to powinno być cztery"};
+            //Setting range for cells to fill
+            Range headlineCellRange = worksheet.Range["A1:F1"];
+            //Array with column headlines
+            string[] headlines = new[] {"Name", "Surname", "ID", "Amount", "Cost", "Unit price"};
 
-            cellRange.set_Value(XlRangeValueDataType.xlRangeValueDefault, strings);
+            //Filling cells with values from array and setting font to bold to show that those are headlines
+            headlineCellRange.set_Value(XlRangeValueDataType.xlRangeValueDefault, headlines);
+            headlineCellRange.Style.Font.Bold = true;
 
-            workbook.SaveAs("RandomExcelFile");
+            //Creating path for folder, saving and closing app
+            string excelFilePath = Path.Combine(folderPath, "RandomExcelFile.xlsx");
+
+            workbook.SaveAs(excelFilePath);
             workbook.Close();
 
         }
